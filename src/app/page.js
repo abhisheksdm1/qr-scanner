fbe2aaba133f203b094de68cec8188ac48025dab
abhishek <abhishek@maitretech.com>
2023-09-08 22:55:25 +0530

    8th commit qr scanner

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,10 @@
 'use client'
-import Image from 'next/image'
-import { Html5QrcodeScanner , Html5QrcodeSupportedFormats } from 'html5-qrcode'
+import Image from 'next/image';
+import { Html5QrcodeScanner, Html5QrcodeSupportedFormats } from 'html5-qrcode';
 import { useEffect, useState } from 'react';
 
-
-
-
 export default function Home() {
-  const [scanResult,setScanResult]=useState(null)
+  const [scanResult, setScanResult] = useState(null);
 
   const formatsToSupport = [
     Html5QrcodeSupportedFormats.QR_CODE,
@@ -15,50 +12,66 @@ export default function Home() {
     Html5QrcodeSupportedFormats.UPC_E,
     Html5QrcodeSupportedFormats.UPC_EAN_EXTENSION,
   ];
-  
 
+  useEffect(() => {
+    // Request camera permission
+    const requestCameraPermission = async () => {
+      try {
+        const stream = await navigator.mediaDevices.getUserMedia({ video: true });
+        startQRCodeScanner(stream); // Start the QR code scanner after permission is granted
+      } catch (error) {
+        console.warn(error);
+        alert('Camera access denied or an error occurred while accessing the camera.');
+      }
+    };
 
-  useEffect(()=>{
-  const scanner = new Html5QrcodeScanner('reader',
-  {
-    qrbox :{
-      width:250,
-      height:250
-    },
-    fps:5,
-    formatsToSupport: formatsToSupport
-   });
+    requestCameraPermission();
+  }, []);
 
-   function onScanSuccess(decodedText, decodedResult) {
-    // Handle on success condition with the decoded text or result.
-    console.log(`Scan result: ${decodedText}`, decodedResult);
-    // ...
-    html5QrcodeScanner.clear();
-    // ^ this will stop the scanner (video feed) and clear the scan area.
-}
-   
-   const success =(result)=>{
-    scanner.clear();
-    setScanResult(result);
-  }
+  const startQRCodeScanner = (stream) => {
+    const scanner = new Html5QrcodeScanner('reader', {
+      qrbox: {
+        width: 250,
+        height: 250,
+      },
+      fps: 5,
+      formatsToSupport: formatsToSupport,
+    });
 
- function error(err)
- {
-  console.warn(err)
- }
-   scanner.render(success,error,onScanSuccess);
+    function onScanSuccess(decodedText, decodedResult) {
+      // Handle on success condition with the decoded text or result.
+      console.log(`Scan result: ${decodedText}`, decodedResult);
+      // ...
+      scanner.clear();
+      // ^ this will stop the scanner (video feed) and clear the scan area.
+    }
 
-  },[])
-  
+    const success = (result) => {
+      scanner.clear();
+      setScanResult(result);
+    };
+
+    scanner.render(success, null, onScanSuccess);
+
+    // Clean up when the component unmounts
+    return () => {
+      scanner.clear();
+      scanner.stop();
+      stream.getTracks().forEach((track) => track.stop()); // Stop the camera stream
+    };
+  };
 
   return (
     <main className="flex min-h-screen flex-col items-center p-24">
-    <h1 className='w-full bg-orange-500 pt-4 pb-4 text-center text-white'>QR code scanner in react</h1>
-    <br/>
-    {
-
-      scanResult ?<div><input type='text'className='p-3 rounded bg-green-600 text-white' value={scanResult}></input></div> :<div id="reader"></div>
-    }
+      <h1 className="w-full bg-orange-500 pt-4 pb-4 text-center text-white">QR code scanner in react</h1>
+      <br />
+      {scanResult ? (
+        <div>
+          <input type="text" className="p-3 rounded bg-green-600 text-white" value={scanResult}></input>
+        </div>
+      ) : (
+        <div id="reader"></div>
+      )}
     </main>
-  )
+  );
 }
